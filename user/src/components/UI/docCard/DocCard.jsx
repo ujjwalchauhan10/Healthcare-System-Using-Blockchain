dca1b365a77ac72b90a8a5aa17ae1b5650128a6c
ujjwalchauhan10 <ujjwalchauhan247@gmail.com>
2023-11-29 19:10:05 +0530

    Added get reports for user.

---
+++ user/src/components/UI/docCard/DocCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "../Button";
+
+const DocCard = ({ uri }) => {
+  console.log(uri);
+  return (
+    <div>
+      <a href={uri} target="_blank" rel="noopener noreferrer">
+        <Button
+          variant="contained"
+          className="rounded"
+          buttonText={"Report"}
+          //onClick={}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default DocCard;
